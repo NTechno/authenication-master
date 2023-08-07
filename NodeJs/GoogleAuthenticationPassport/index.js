292ff9e760e7f4251f501882b746823571272545
Nidhi Gadhavi <gadhavi019@gmail.com>
2023-08-07 18:12:48 +0530

    initial commit

--- NodeJs/GoogleAuthenticationPassport/index.js
+++ NodeJs/GoogleAuthenticationPassport/index.js
@@ -1,67 +1,76 @@
 /*  EXPRESS */
-const express = require('express');
+const express = require("express");
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(session({
-  resave: false,
-  saveUninitialized: true,
-  secret: 'SECRET' 
-}));
+app.use(
+	session({
+		resave: false,
+		saveUninitialized: true,
+		secret: "SECRET",
+	})
+);
 
-app.get('/', function(req, res) {
-  res.render('pages/auth');
+app.get("/", function (req, res) {
+	res.render("pages/auth");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port , () => console.log('App listening on port ' + port));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("App listening on port " + port));
 
-
-var passport = require('passport');
+var passport = require("passport");
 var userProfile;
- 
+
 app.use(passport.initialize());
 app.use(passport.session());
- 
-app.get('/success', (req, res) => {
-  res.render('pages/success', {user: userProfile});
+
+app.get("/success", (req, res) => {
+	res.render("pages/success", { user: userProfile });
 });
-app.get('/error', (req, res) => res.send("error logging in"));
- 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
- 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+app.get("/error", (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function (user, cb) {
+	cb(null, user);
 });
 
+passport.deserializeUser(function (obj, cb) {
+	cb(null, obj);
+});
 
 /*  Google AUTH  */
- 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = 'our-google-client-id';
-const GOOGLE_CLIENT_SECRET = 'our-google-client-secret';
 
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-  }
-));
- 
-app.get('/auth/google', 
-  passport.authenticate('google', { scope : ['profile', 'email'] }));
- 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    // Successful authentication, redirect success.
-    res.redirect('/success');
-  });
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+const GOOGLE_CLIENT_ID =
+	"147737003183-6vsu446gtiu1lt9ru4adulaeh2hi1pj9.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:5000/auth/google/callback",
+		},
+		function (accessToken, refreshToken, profile, done) {
+			console.log("into google stretegy!!");
+			userProfile = profile;
+			return done(null, userProfile);
+		}
+	)
+);
+
+app.get(
+	"/auth/google",
+	passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+	"/auth/google/callback",
+	passport.authenticate("google", { failureRedirect: "/error" }),
+	function (req, res) {
+		// Successful authentication, redirect success.
+		res.redirect("/success");
+	}
+);
--- NodeJs/OauthGithub/index.js
+++ NodeJs/OauthGithub/index.js
@@ -1,6 +1,9 @@
 /*  EXPRESS */
 const express = require('express');
 const app = express();
+var passport = require("passport");
+var GitHubStrategy = require('passport-github2').Strategy;
+
 
 app.set('view engine', 'ejs');
 var access_token = "";
@@ -13,33 +16,68 @@ const port = process.env.PORT || 2400;
 app.listen(port , () => console.log('App listening on port ' + port));
 
 
+passport.serializeUser(function (user, cb) {
+	cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+	cb(null, obj);
+});
+
+
 
 
 // Import the axios library, to make HTTP requests
 const axios = require('axios')
 // This is the client ID and client secret that you obtained
 // while registering on github app
-const clientID = '9e15217cce244512f12e'
-const clientSecret = '4b22b19305b352254c0c9d6170035264b9a73f63'
+const clientID = '5f731f125d74ef7d80d8'
+const clientSecret = '7bf02b5b82b47b2f6607a4983100ee315279217a'
 
-// Declare the callback route
-app.get('/github/callback', (req, res) => {
+passport.use(new GitHubStrategy({
+  clientID: clientID,
+  clientSecret: clientSecret,
+  callbackURL: "http://localhost:2400/auth/github/callback"
+},
+function(accessToken, refreshToken, profile, done) {
+  console.log("into the stretegy github" , profile);
+  // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+  //   return done(err, user);
+  // });
+}
+));
 
-  // The req.query object has the query params that were sent to this route.
-  const requestToken = req.query.code
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    console.log("into the github callback auth!!!!!" , res);
+    access_token = res.data.access_token
+
+    // res.redirect('/success');
+  });
+
+// // Declare the callback route
+// app.get('/github/callback', (req, res) => {
+//   console.log("git hub login callback");
+//   // The req.query object has the query params that were sent to this route.
+//   const requestToken = req.query.code
   
-  axios({
-    method: 'post',
-    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-    // Set the content type header, so that we get the response in JSON
-    headers: {
-         accept: 'application/json'
-    }
-  }).then((response) => {
-    access_token = response.data.access_token
-    res.redirect('/success');
-  })
-})
+//   axios({
+//     method: 'post',
+//     url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
+//     // Set the content type header, so that we get the response in JSON
+//     headers: {
+//          accept: 'application/json'
+//     }
+//   }).then((response) => {
+//     access_token = response.data.access_token
+//     res.redirect('/success');
+//   })
+// })
 
 app.get('/success', function(req, res) {
 
